91604fa9dc5fae6cc7873882ffd7312ea8f7712c
Diegosaelices <saelicessaelicesdiego@gmail.com>
2024-09-18 16:48:10 +0200

    comit de comentario

--- src/main/java/com/iesjuanbosco/ejemploweb/App.java
+++ src/main/java/com/iesjuanbosco/ejemploweb/App.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
-
+	//comenatrio de ejemplo
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
